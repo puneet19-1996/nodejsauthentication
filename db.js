8e2a1a20eadb91cbcf64740a5c5a41725e8460c3
puneet19-1996 <70681384+puneet19-1996@users.noreply.github.com>
2024-01-21 10:27:54 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //Connect app with database
-const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/authentication";
+const MONGO_URI = "mongodb+srv://puneet:puneet@cluster0.wacuc1r.mongodb.net/";
 mongoose.connect(MONGO_URI). 
 then( ()=> console.log('Connection to database')). 
 catch((e) => console.error('Error occured during connection to database ',e))
